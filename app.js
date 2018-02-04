1e297ecb68086bd8b1c688d70648ec9b1d13d284
Geonho Kim <gunhoflash@gmail.com>
2018-02-04 21:52:40 +0900

    git init

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
+const port = process.env.PORT || 5000;
+
+// ejs
+app.set('view engine', 'ejs');
+app.set('views', '/views');
+
+app.locals.pretty = true;
+app.use(express.static('public'));
+app.use('/public', express.static('public'));
+
+// route
+app.get('/', (req, res) =>
+{
+	res.render('main');
+});
+
+// listen
+app.listen(port, () =>
+{
+	console.log("Connected port" + port);
+});
