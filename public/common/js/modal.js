7ed2e6ea91da58e629d699173779e291decc91c8
희진 <gml6162>
2018-03-12 16:54:18 +0900

    capture

--- public/common/js/modal.js
+++ public/common/js/modal.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
-	//$(".table tr").click(function(){
 	$(".btn.btn-primary").click(function(){
-		/*var str = "hello"
-		var tr = $(this);
-		var td = tr.children();
-		td.each(function(i){
-			console.log(td.eq(i).text("hello"));
-		})*/
 		var string = "<div class=\"class-box\" style=\"height:";
 		var name = $("#lecture").val();
 		var day = $("#inputDay option:selected").val();
@@ -23,5 +16,14 @@ $(document).ready(function(){
 		string += "</div>";
 		timetable.rows[start].cells[day].innerHTML = string;
 		console.log(time);
+	})
+
+	$('#save').click(function(){
+		console.log("html2canvas");
+		html2canvas($('#timetable').get(0)).then(function(canvas)
+		{
+			var img = canvas.toDataURL("image/png", 1);
+			$('#img').attr('src', img);
+		})
 	})
 })
